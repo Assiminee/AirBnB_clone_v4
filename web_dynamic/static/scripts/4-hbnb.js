8cb9517a1a4541aca381f242b5c37e62efb59620
Sandy Obare <obaresandy@gmail.com>
2024-08-11 22:15:28 +0300

    4-hbnb.js update: based on 3-hbnb.js: When the button tag is clicked, a new POST request to places_search should be made with the list of Amenities checked

---
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -0,0 +1,96 @@
+/* global $ */
+const checked = () => {
+  const selectedAmenities = {};
+  $('.amenities input[type="checkbox"]').change(function () {
+    const id = $(this).attr('data-id');
+    const name = $(this).attr('data-name');
+
+    if ($(this).is(':checked')) {
+      selectedAmenities[`${id}`] = name;
+    } else {
+      delete selectedAmenities[`${id}`];
+    }
+
+    const amenities = Object.values(selectedAmenities).sort().join(', ');
+
+    $('.amenities h4').text(amenities);
+  });
+};
+
+const checkStatus = () => {
+  const endpoint = 'http://127.0.0.1:5001/api/v1/status';
+
+  fetch(endpoint, { method: 'GET', headers: { Accept: 'application/json', 'Content-Type': 'application/json' } })
+    .then(response => response.json())
+    .then(data => {
+      if (data.status === 'OK') {
+        $('#api_status').addClass('available');
+      } else {
+        $('#api_status').removeClass('available');
+      }
+    })
+    .catch(() => {
+      $('#api_status').removeClass('available');
+    });
+};
+
+const searchPlaces = (requestBody) => {
+  const endpoint = 'http://127.0.0.1:5001/api/v1/places_search/';
+
+  fetch(endpoint, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(requestBody)
+  })
+    .then(response => response.json())
+    .then(data => {
+      data.forEach(obj => {
+        const article = $('<article></article>').appendTo('.places');
+
+        const titleBox = $('<div class="title_box"></div>').appendTo(article);
+        titleBox.append(`<h2>${obj.name}</h2>`);
+        titleBox.append(`<div class="price_by_night">${obj.price_by_night}</div>`);
+
+        const information = $('<div class="information"></div>').appendTo(article);
+        information.append(`<div class="max_guest">${obj.max_guest} ${obj.max_guest === 1 ? 'Guest' : 'Guests'}</div>`);
+        information.append(`<div class="number_rooms">${obj.number_rooms} ${obj.number_rooms === 1 ? 'Bedroom' : 'Bedrooms'}</div>`);
+        information.append(`<div class="number_bathrooms">${obj.number_bathrooms} ${obj.number_bathrooms === 1 ? 'Bathroom' : 'Bathrooms'}</div>`);
+
+        article.append(`<div class="description">${obj.description}</div>`);
+      });
+    })
+    .catch(() => {});
+};
+
+const filterAmenities = () => {
+  const requestBody = {
+    amenities: []
+  };
+
+  $('.amenities input[type="checkbox"]').change(function () {
+    const id = $(this).attr('data-id');
+
+    if ($(this).is(':checked')) {
+      requestBody.amenities.push(id);
+    } else {
+      const idx = requestBody.amenities.indexOf(id);
+      if (idx >= 0) {
+        requestBody.amenities.splice(idx, 1);
+      }
+    }
+  });
+
+  $('button').on('click', function () {
+    $('.places').empty();
+    searchPlaces(requestBody);
+  });
+};
+
+$(document).ready(function () {
+  checked();
+  checkStatus();
+  searchPlaces({});
+  filterAmenities();
+});
